a978eb47d19efc24fb22dbe424d3b8b954534af4
MrKanYT <mrkan2017@gmail.com>
2022-10-18 18:06:45 +0400

    начало переноса на lava business

--- db.py
+++ db.py
@@ -15,39 +15,64 @@ class UserNotFoundException(Exception):
     pass
 
 
+class InvoiceNotFoundException(Exception):
+    """
+    Счет не найден в базе данных.
+    """
+    pass
+
+
+class InvoiceInfo:
+    """
+    Содержит информацию о счете.
+    """
+    order_id: str    # номер счета
+    creator: str   # токен пользователя, для которого создан счет
+    status: str    # статус счета
+    amount: float    # сумма счета
+    credited: float    # сумма, зачисленная после оплаты счета
+    created: str    # время создания счета
+    payed: str    # время оплаты счета
+    auto_withdraw: bool    # автовывод средств при True
+    comment: str    # комментарий
+
+    def __init__(self, fields: Tuple):
+        if len(fields) != 9:
+            raise ValueError(f"Ожидаемое количество элементов в fields: 9. Получено: {len(fields)}")
+
+        self.order_id, self.creator, self.status, self.amount, self.credited, self.created, \
+            self.payed, self.auto_withdraw, self.comment = fields
+
+
 class UserInfo:
     """
     Содержит информацию о пользователе.
     """
     token: str    # уникальный токен пользователя
-    shop_id: str    # айди ммагазина на Lava
     name: str    # имя (в коде не используется. Нужно исключительно для понятности в БД.)
     percent: int    # процент от оплаченых счетов, который будет автоматически выводиться
     withdraw_service: str    # способ вывода (https://dev.lava.ru/methods) + 'lava' для перевода на другой лава кошелек
     withdraw_wallet: str    # номер счета, на который производится вывод. Подробнее: https://dev.lava.ru/withdrawcreate
 
-    def __init__(self, fields: Tuple = None, token: str = None, shop_id = None, name: str = None,
+    def __init__(self, fields: Tuple = None, token: str = None, name: str = None,
                  percent: int = None, withdraw_service: str = None, withdraw_wallet: str = None):
         """
         Создает экземпляр класса с данными пользователя по заданым параметрам.
 
         :param fields: Кортеж данных, получаемых из БД. Порядок: token, name, percent, withdraw_service, withdraw_wallet.
         :param token: Токен пользователя. Перезаписывает токен из fields.
-        :param shop_id: Айди проетка на Lava. Перезаписывает айди из fields.
         :param name: Имя пользователя. Перезаписвает имя из fields.
         :param percent: Процент, получаемый пользователем. Перезаписывает процент из fields.
         :param withdraw_service: Способ авто-вывода средств. Перезаписывает сервис из fields.
         :param withdraw_wallet: Номер счета для авто-вывода. Перезаписывает номер счета из fields.
         """
         # распаковываем данные из fields
-        if len(fields) == 6:
-            self.token, self.shop_id, self.name, self.percent, self.withdraw_service, self.withdraw_wallet = fields
+        if len(fields) == 5:
+            self.token, self.name, self.percent, self.withdraw_service, self.withdraw_wallet = fields
 
         # если параметр указан, то перезаписываем параметр из fields
         if token is not None:
             self.token = token
-        if shop_id is not None:
-            self.shop_id = shop_id
         if name is not None:
             self.name = name
         if percent is not None:
@@ -71,7 +96,48 @@ class DatabaseManager:
         self.connection = sqlite3.connect(filename)
         self.cursor = self.connection.cursor()
 
-    def get_user_info(self, token: str):
+    def get_invoice_info(self, order_id: str) -> InvoiceInfo:
+        """
+        Находит информацию о счете по айди.
+        :param order_id: Айди счета
+        :return: Данные о счете.
+        """
+        print(order_id)
+        self.cursor.execute("SELECT * FROM invoices WHERE order_id = ?", (order_id,))
+
+        rows = self.cursor.fetchall()
+
+        # если список строк пуст, значит счет не найден
+        if not rows:
+            raise InvoiceNotFoundException(f"Счет с айди {order_id} не найден.")
+
+        invoice_info = InvoiceInfo(rows[0])
+
+        return invoice_info
+
+    def save_invoice_info(self, invoice_info: InvoiceInfo):
+        """
+        Сохраняет данные о счете.
+        :param invoice_info: Данные о счете, которые нужно сохранить
+        :return:
+        """
+        try:
+            self.cursor.execute(
+                "INSERT INTO invoices (order_id, creator, status, amount, credited, created, payed, auto_withdraw, comment) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);",
+                (invoice_info.order_id, invoice_info.creator, invoice_info.status, invoice_info.amount,
+                 invoice_info.credited, invoice_info.created, invoice_info.payed,  int(invoice_info.auto_withdraw),
+                 invoice_info.comment))
+            "ON DUPLICATE KEY UPDATE "
+        except sqlite3.IntegrityError:
+            self.cursor.execute(
+                "UPDATE invoices SET order_id = ?, creator = ?, status = ?, amount = ?, credited = ?, created = ?, payed = ?, auto_withdraw = ?, comment = ? WHERE order_id = ?;",
+                (invoice_info.order_id, invoice_info.creator, invoice_info.status, invoice_info.amount,
+                 invoice_info.credited, invoice_info.created, invoice_info.payed, int(invoice_info.auto_withdraw),
+                 invoice_info.comment, invoice_info.order_id))
+        self.connection.commit()
+
+
+    def get_user_info(self, token: str) -> UserInfo:
         """
         Находит информацию о пользователе по токену.
 
@@ -98,25 +164,36 @@ class DatabaseManager:
 def __create_table(connection: Connection, cursor: Cursor):
     cursor.execute(
         "CREATE TABLE IF NOT EXISTS users "
-        "(token VARCHAR(32) NOT NULL, shop_id VARCHAR(32) NOT NULL, name VARCHAR(32) NOT NULL, percent INT UNSIGNED NOT NULL, "
+        "(token VARCHAR(32) NOT NULL, name VARCHAR(32) NOT NULL, percent INT UNSIGNED NOT NULL, "
         "withdraw_service VARCHAR(16) NOT NULL, withdraw_wallet VARCHAR(32), PRIMARY KEY (token, name));")
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS invoices "
+        "(order_id VARCHAR(32) NOT NULL, creator VARCHAR(32) NOT NULL, status VARCHAR(32) NOT NULL, "
+        "amount REAL NOT NULL, credited REAL NOT NULL, created VARCHAR(32) NOT NULL, "
+        "payed VARCHAR(32) NOT NULL, auto_withdraw BIT NOT NULL, "
+        "comment VARCHAR(32) NOT NULL, PRIMARY KEY (order_id));")
     connection.commit()
 
 
 def __drop_table(connection: Connection, cursor: Cursor):
-    cursor.execute("DROP TABLE IF EXISTS users;")
+    cursor.execute("DROP TABLE IF EXISTS invoices;")
     connection.commit()
 
 
-def __add_user(connection: Connection, cursor: Cursor, name: str, shop_id: str, percent: int, withdraw_service: str, withdraw_wallet: str, token: str = None):
+def __add_user(connection: Connection, cursor: Cursor, name: str, percent: int, withdraw_service: str, withdraw_wallet: str, token: str = None):
     if token is None:
         token = secrets.token_urlsafe(32)    # генерирует токен из случайных символов длиной 32 символа
 
-    cursor.execute("INSERT INTO users (token, shop_id, name, percent, withdraw_service, withdraw_wallet) VALUES (?, ?, ?, ?, ?, ?);",
-                   (token, name, shop_id, percent, withdraw_service, withdraw_wallet))
+    cursor.execute("INSERT INTO users (token, name, percent, withdraw_service, withdraw_wallet) VALUES (?, ?, ?, ?, ?);",
+                   (token, name, percent, withdraw_service, withdraw_wallet))
     connection.commit()
 
     return token
+
+
+def __edit_user(connection: Connection, cursor: Cursor):
+    cursor.execute("UPDATE users SET withdraw_service='lava', withdraw_wallet='R10135783' WHERE name = 'TestUser';")
+    connection.commit()
 
 
 def __get_all_users(connection: Connection, cursor: Cursor):
@@ -125,10 +202,22 @@ def __get_all_users(connection: Connection, cursor: Cursor):
     return cursor.fetchall()
 
 
+def __get_all_invoices(connection: Connection, cursor: Cursor):
+    cursor.execute("SELECT * FROM invoices")
+
+    return cursor.fetchall()
+
+
 if __name__ == "__main__":
     dbm = DatabaseManager("database.sqlite3")
     #__drop_table(dbm.connection, dbm.cursor)
     #__create_table(dbm.connection, dbm.cursor)
-    #print(__add_user(dbm.connection, dbm.cursor, "123-shop-id-321", "SOZ", 100, "qiwi", "+79608357711"))
-    dbm.get_user_info("0WH5bBJrq2nA1x6Wpk85vxBvOpC5zLydSuj9YIcOPa8")
+    #print(__add_user(dbm.connection, dbm.cursor, "LavaWithdraw", 100, "lava", "R10135783"))
+    #dbm.get_user_info("-RKZE_bga7T-27GkEowwR6JqRWfJ9mzVkUW3u0g-1-w")
+    #ii = InvoiceInfo(("123456-123-1234-123456", "-RKZE_bga7T-27GkEowwR6JqRWfJ9mzVkUW3u0g-1-w", "created",
+                      #20, 19, "1:1:1 1:1:1", "2:3:1 1:1:1", True, "Comment"))
+    #dbm.save_invoice_info(ii)
+    #print(dbm.get_invoice_info("ae8c2701-8f8b-1de9-08a3-d2fce55ddf4e"))
+    #__edit_user(dbm.connection, dbm.cursor)
     print(__get_all_users(dbm.connection, dbm.cursor))
+    print(__get_all_invoices(dbm.connection, dbm.cursor))
--- main.py
+++ main.py
@@ -2,18 +2,21 @@
 Основной файл.
 Команда для запуска: uvicorn main:app --reload
 """
-
 import datetime
-import os
-import random
 
+"""
+{'invoice_id': 'b0491e7e-590e-9bea-bb64-44432f62dbdd', 'status': 'success', 'pay_time': 1665961760, 'amount': '20.00', 'order_id': None, 'pay_service': 'qiwi', 'payer_details': None, 'custom_fields': '', 'type': 1, 'credited': '19.00', 'merchant_id': 'TestPayment'}
+"""
+
+import os
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
+import dlltool
 import db as database
 import logging
 from typing import Optional
 import asyncio
-import lava_api
+from typing import Dict, Any
 
 
 class CreateInvoiceRequest(BaseModel):
@@ -23,6 +26,8 @@ class CreateInvoiceRequest(BaseModel):
     user_token: str    # токен пользователя
     amount: int    # сумма счета
     expire: int    # время жизни счета (по каким-то причинам не работает)
+    comment: Optional[str] = Field("")   # комментарий
+    auto_withdraw: Optional[bool] = Field(True)  # авто-вывод
 
 
 class InvoiceStatusRequest(BaseModel):
@@ -31,12 +36,11 @@ class InvoiceStatusRequest(BaseModel):
     """
     user_token: str    # токен пользователя
     id: str    # айди счета, состояние которого необходимо получить
-    auto_withdraw: Optional[str] = Field(False)   # необязательный параметр. Если True, то после оплаты счета средства будут автоматически выведены
+    auto_withdraw: Optional[bool] = Field(False)   # необязательный параметр. Если True, то после оплаты счета средства будут автоматически выведены
 
 
 app = FastAPI(docs_url=None, redoc_url=None)    # docs_url и redoc_url отключают автоматическую документацию
-#SECRET_KEY = "234567899"
-SECRET_KEY = "1ffe6afc-01aa-4da9-b1e7-6f2ce521e506"
+TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1aWQiOiIxZmQwZDZlYi02YjZlLTVkYjYtM2IzYS1lNzk0ZmJlZTRiMGYiLCJ0aWQiOiJjNjdmYWFmMS1jYTIxLWJiMzQtOTA5Yi1lYjg1MDI1OGExOWMifQ.lkRc2IPMT2m_eWnP18fbq90J9EX1tQJzrGScrX66K-U"
 WALLET = "R10031991"
 db = database.DatabaseManager("database.sqlite3")    # экземпляр класса для доступа к данным из БД.
 
@@ -55,14 +59,15 @@ class WithdrawException(Exception):
     pass
 
 
-async def withdraw(user: database.UserInfo, amount: int) -> None:
+async def withdraw(user: database.UserInfo, amount: float, comment: str) -> None:
     """
     Выводит средства
     :param user: Информация о пользователе из БД
     :param amount: Сумма
+    :param comment: Комментарий
     :return:
     """
-
+    amount = float(amount)
     logger.info(f"Withdraw requested: user = {user.name}; service: {user.withdraw_service}; "
                 f"wallet: {user.withdraw_wallet}; amount = {amount}")
 
@@ -72,10 +77,12 @@ async def withdraw(user: database.UserInfo, amount: int) -> None:
         fields = {
             "account_from": WALLET,
             "account_to": user.withdraw_wallet,
-            "amount": f"{amount * (user.percent / 100):.2f}"
+            "amount": f"{amount * (user.percent / 100):.2f}",
+            "comment": comment
+
         }
         logger.info(f"Sended transfer request with fields: {fields}")
-        response = dlltool.send("https://api.lava.ru/transfer/create", "POST", {"Authorization": SECRET_KEY},
+        response = dlltool.send("https://api.lava.ru/transfer/create", "POST", {"Authorization": TOKEN},
                                 fields)  # отправка данных в API (см. описание модуля)
 
     # иначе создаем вывод средств
@@ -88,7 +95,7 @@ async def withdraw(user: database.UserInfo, amount: int) -> None:
             "wallet_to": user.withdraw_wallet
         }
         logger.info(f"Sended withdraw request with fields: {fields}")
-        response = dlltool.send("https://api.lava.ru/withdraw/create", "POST", {"Authorization": SECRET_KEY},
+        response = dlltool.send("https://api.lava.ru/withdraw/create", "POST", {"Authorization": TOKEN},
                                 fields)  # отправка данных в API (см. описание модуля)
 
     if (status := response.get("status", "error")) != "success":
@@ -98,11 +105,78 @@ async def withdraw(user: database.UserInfo, amount: int) -> None:
     logger.info(f"[WITHDRAW] Withdraw request successfully created: \nRequest: {fields}; \nResponse: {response};")
 
 
+@app.post("/payment_service/webhook")
+async def webhook(data: Dict[Any, Any]):
+    logger.info(f"[WEBHOOK] Received: {data}")
+    try:
+        invoice_info = db.get_invoice_info(data.get("invoice_id", "UNDEFINED"))
+    except database.InvoiceNotFoundException:
+        return
+    if invoice_info.status != "created":
+        return
+
+    invoice_info.credited = float(data.get("credited", -1))
+    invoice_info.payed = str(datetime.datetime.now())
+    invoice_info.status = "payed"
+
+    db.save_invoice_info(invoice_info)
+
+    logger.info(f"[WEBHOOK] Invoice payed: {invoice_info.order_id} {invoice_info.creator} {invoice_info.comment}")
+
+    if invoice_info.auto_withdraw and invoice_info.credited != -1:
+        try:
+            user = db.get_user_info(invoice_info.creator)
+        except database.UserNotFoundException:
+            logger.error(f"User with token {invoice_info.creator} not found")
+            return {
+                "status": "server error",
+                "code": "-1",
+                "message": "Invalid user token"
+            }
+
+        try:
+            await withdraw(user, invoice_info.credited,
+                           invoice_info.comment)  # создание запроса на вывод средств
+            invoice_info.status = "withdrawed"
+            db.save_invoice_info(invoice_info)
+        except WithdrawException as ex:  # API вернул статус "error"
+            withdraw_status = "error"
+            withdraw_message = ex.args[0]
+            return {"status": withdraw_status, "message": withdraw_message}
+
+
 @app.post("/payment_service/get_invoice_status")
 async def get_invoice_status(invoice_status_request: InvoiceStatusRequest):
 
-    logger.info(f"Get invoice status requested: {invoice_status_request}")
 
+    try:
+        invoice_info = db.get_invoice_info(invoice_status_request.id)
+    except database.InvoiceNotFoundException:
+        return {
+            "status": "server error",
+            "code": "-2",
+            "message": "Invalid invoice id"
+        }
+
+    if invoice_info.status == 'payed':
+        return {
+            "status": "success",
+            "withdraw_status": "waiting" if invoice_info.auto_withdraw else "disabled",
+            "withdraw_message": ""
+        }
+    elif invoice_info.status == "withdrawed":
+        return {
+            "status": "success",
+            "withdraw_status": "success",
+            "withdraw_message": ""
+        }
+    elif invoice_info.status == "created":
+        return {
+            "status": "pending",
+            "withdraw_status": "disabled",
+            "withdraw_message": ""
+        }
+    """
     # если пользователь с полученым токеном не найден, то возвращает ошибку
     try:
         user = db.get_user_info(invoice_status_request.user_token)
@@ -120,7 +194,7 @@ async def get_invoice_status(invoice_status_request: InvoiceStatusRequest):
 
     logger.debug(f"Sending invoice status request with fields: {fields}")
 
-    response = dlltool.send("https://api.lava.ru/invoice/info", "POST", {"Authorization": SECRET_KEY}, fields)    # отправка данных в API (см. описание модуля)
+    response = dlltool.send("https://api.lava.ru/invoice/info", "POST", {"Authorization": TOKEN}, fields)    # отправка данных в API (см. описание модуля)
 
     logger.debug(f"Got response: {response}")
 
@@ -146,19 +220,9 @@ async def get_invoice_status(invoice_status_request: InvoiceStatusRequest):
 
         # если счет оплачен и в запросе включен авто-вывод
         if invoice_status == "success" and invoice_status_request.auto_withdraw:
-            try:
-                await withdraw(user, int(float(invoice_info.get("sum", "0"))))    # создание запроса на вывод средств
-                withdraw_status = "success"
-                withdraw_message = ""
-            except WithdrawException as ex:    # API вернул статус "error"
-                withdraw_status = "error"
-                withdraw_message = ex.args[0]
 
-        return {
-            "status": invoice_status,
-            "withdraw_status": withdraw_status,
-            "withdraw_message": withdraw_message
-        }
+
+
 
     # при получении статуса счета произошла ошибка
     else:
@@ -169,17 +233,7 @@ async def get_invoice_status(invoice_status_request: InvoiceStatusRequest):
             "code": response.get("code", "UNDEFINED"),
             "message": response.get("message", "UNDEFINED")
         }
-
-
-def generate_invoice_id(user_name: str) -> str:
     """
-    Генерирует уникальный идентификатор счета
-
-    :param user_name: Имя продавца, который выставляет счет
-    :return: SHA256 хеш
-    """
-
-    return f"{user_name}-{datetime.datetime.now().strftime('%d-%m-%Y-%H-%M-%S')}-{random.randint(0, 256)}"
 
 @app.post("/payment_service/create_invoice/")
 async def create_invoice(invoice_request: CreateInvoiceRequest):
@@ -207,16 +261,32 @@ async def create_invoice(invoice_request: CreateInvoiceRequest):
             "message": "Invalid user token"
         }
 
-    response = lava_api.create_invoice(SECRET_KEY,
-                                       invoice_request.amount,
-                                       generate_invoice_id(user.name),
-                                       SECRET_KEY)
-    print(response)
+    # см. https://dev.lava.ru/invoicecreate
+    fields = {
+        "wallet_to": WALLET,
+        "sum": f"{invoice_request.amount:.2f}",
+        "expire": "600",
+        "merchant_id": user.name,
+        "comment": invoice_request.comment,
+        "hook_url": "http://185.189.255.220:8100/payment_service/webhook"
+    }
+
+    logger.debug(f"Sending create invoice request with fields: {fields}")
+
+    response = dlltool.send("https://api.lava.ru/invoice/create", "POST", {"Authorization": TOKEN}, fields)    # отправка данных в API (см. описание модуля)
+
+    logger.debug(f"Got response: {response}")
+
     # счет успешно выставлен
     if (status := response.get("status", "error")) == "success":
-        data: dict = response.get("data", {})
-        logger.info(f"Invoice created successfully: id = {data.get('id', 'UNDEFINED')}; "
-                    f"sum = {data.get('amount', 'UNDEFINED')}; url = {data.get('url', 'UNDEFINED')}")
+
+        logger.info(f"Invoice created successfully: id = {response.get('id', 'UNDEFINED')}; "
+                    f"sum = {invoice_request.amount}; url = {response.get('url', 'UNDEFINED')}")
+
+        invoice_info = database.InvoiceInfo((response.get('id', 'UNDEFINED'), user.token, 'created',
+                                             invoice_request.amount, -1, str(datetime.datetime.now()), "",
+                                             invoice_request.auto_withdraw, invoice_request.comment))
+        db.save_invoice_info(invoice_info)
 
         return {
             "status": status,
@@ -226,24 +296,24 @@ async def create_invoice(invoice_request: CreateInvoiceRequest):
     # ошибка при выставлении счета
     else:
 
-        logger.error(f"[API ERROR] Error while creating invoice: \nResponse: {response}")
+        logger.error(f"[API ERROR] Error while creating invoice: \nRequest fields: {fields} \nResponse: {response}")
 
         return {
             "status": status,
-            "code": response.get("status", "UNDEFINED"),
-            "message": response.get("error", {"UNDEFINED": [""]})
+            "code": response.get("code", "UNDEFINED"),
+            "message": response.get("message", "UNDEFINED")
         }
 
 
 # только для тестирования
 async def main():
-    test_invoice_creation_request = CreateInvoiceRequest(user_token="0WH5bBJrq2nA1x6Wpk85vxBvOpC5zLydSuj9YIcOPa8",
+    test_invoice_creation_request = CreateInvoiceRequest(user_token="2qxm3GWCHnUxSO3e7fWJFcbKRPpmYWEaK7HcPoPxu1M",
                                                          amount=20, expire=60)
     test_invoice_response = await create_invoice(test_invoice_creation_request)
     print(test_invoice_response)
-    '''
+
     test_invoice_status_request = InvoiceStatusRequest(id=test_invoice_response.get("id", "UNDEFINED"),
-                         user_token="vw9NAdhy-vNB0L-hXWY7Z3TEGookB3Eg9kDDeu_hqf0", auto_withdraw=True)
+                         user_token="2qxm3GWCHnUxSO3e7fWJFcbKRPpmYWEaK7HcPoPxu1M", auto_withdraw=True)
 
     tryings_left = 10
     while tryings_left > 0:
@@ -258,8 +328,11 @@ async def main():
         await asyncio.sleep(10)
 
     print("RESULT: ", test_invoice_status)
-'''
-    #user = db.get_user_info("2qxm3GWCHnUxSO3e7fWJFcbKRPpmYWEaK7HcPoPxu1M")
+
+    #user = db.get_user_info("aoTwBen_REaz44vuBtUPovGWHJiweYG1ZdwLrNueMEw")
+    #test_withdraw_response = await withdraw(user, 10, "Hello World!")
+
+
     pass
 
 
